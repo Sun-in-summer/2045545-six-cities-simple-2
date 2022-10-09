402c08c51296c96aee8efc8619a862a6b5642a56
Elena <eamatytsina@gmail.com>
2022-10-09 12:38:56 +0300

    task 2 WIP2

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,26 +1,33 @@
+import {config} from 'dotenv';
+import {inject, injectable} from 'inversify';
 import {ConfigInterface} from './config.interface.js';
-import {config, DotenvParseOutput} from 'dotenv';
 import { LoggerInterface } from '../logger/logger.interface.js';
+import { configSchema, ConfigSchema } from './config.schema.js';
+import { Component } from '../../types/component.types.js';
 
-
+@injectable()
 export default class ConfigService implements ConfigInterface {
-  private config: DotenvParseOutput;
+  private config: ConfigSchema;
   private logger: LoggerInterface;
 
-  constructor(logger: LoggerInterface) {
+  constructor(@inject(Component.LoggerInterface) logger: LoggerInterface) {
     this.logger= logger;
 
     const parsedOutput= config();
+    console.log(parsedOutput);
 
     if (parsedOutput.error) {
       throw new Error('Can\'t read .env file. Perhaps the file does not exist.');
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configSchema.load({});
+    configSchema.validate({allowed: 'strict', output: this.logger.info});
+
+    this.config = configSchema.getProperties();
     this.logger.info('.env file found and successfully parsed');
   }
 
-  public get(key: string): string | undefined {
+  public get<T extends keyof ConfigSchema>(key: T) {
     return this.config[key];
   }
 }
